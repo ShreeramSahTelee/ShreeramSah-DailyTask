0a9214cd993bd946b018bad70d0cad43114553e0
ShreeramSahTelee <shreeramsah5566@gmail.com>
2023-01-08 12:56:20 +0545

    updated

---
+++ day7.java
@@ -0,0 +1,10 @@
+import java.util.Scanner;
+
+public class day7 {
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        System.out.println("Enter your Name:");
+        String name=sc.nextLine();
+        System.out.println("Name="+name);
+    }
+}
