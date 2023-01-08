7edf0dd67fd716f443aeec0e930a283b57ef0889
ShreeramSahTelee <shreeramsah5566@gmail.com>
2023-01-08 13:09:17 +0545

    Updated code

--- day7.java
+++ day7.java
@@ -3,8 +3,18 @@ import java.util.Scanner;
 public class day7 {
     public static void main(String[] args) {
         Scanner sc=new Scanner(System.in);
+        String userName="Pachu";
+        String userPassword="pachu1234@";
         System.out.println("Enter your Name:");
         String name=sc.nextLine();
         System.out.println("Name="+name);
+        System.out.println("Enter your password:");
+        String password=sc.next();
+        if(userName.equals(name)&&userPassword.equals(password)){
+            System.out.println("login sucessfull");
+        }
+        else{
+            System.out.println("you are entering wrong user and password");
+        }
     }
 }
